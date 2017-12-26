6a227c59f1c0bb499c710399d4adab4e801c9541
phyllocrania <34107739+phyllocrania@users.noreply.github.com>
2017-12-26 18:36:54 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+function getQueryVariable(variable) 
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+function addNewEvent() {
+window.newEventName = getQueryVariable('name')
+window.newEventContent = getQueryVariable('content')
+document.getElementById('hcb_form_name').value = window.newEventName;
+document.getElementById("hcb_form_content").value = window.newEventContent;
+document.getElementById("hcb_submit").click();
+}
